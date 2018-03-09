dc16b515bc00d2f07a504671f4eed69168dc6da5
Hugh <hug_hob@hotmail.com>
2018-03-09 18:13:21 +0000

    data needs to be in the form of a strict to write to file

--- dublinbikeminer/dublinbikeminer/dublinbikeminer.py
+++ dublinbikeminer/dublinbikeminer/dublinbikeminer.py
@@ -4,11 +4,13 @@ import time
 
 def main():
     
+    count = 1
+    
     while True:
         
+        print("collecting:",count)
         
-        
-        file = open("/hugh/home/30670/project/dublinbikedata.txt", w)
+        file = open("/hugh/home/30670/Project/dublinbikedata.txt", w)
         
         link = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&&apiKey=4dc48c410fefd7a42d52cdc4a9c6eb7ce0f67ae0"
         
@@ -23,6 +25,8 @@ def main():
         time.sleep(300)
         
         #print(json.dumps(parsed, indent=4))
+        
+        count+=1
         
     
 
